41b9460bd64431c519940b0876303d5da2b6c868
ote2010 <ote2010@naver.com>
2016-09-03 14:08:20 +0900

    다이얼로그 구현

--- Travel360/app/src/main/java/com/example/user/travel360/StoryWriteActivity.java
+++ Travel360/app/src/main/java/com/example/user/travel360/StoryWriteActivity.java
@@ -2,10 +2,12 @@ package com.example.user.travel360;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
@@ -81,7 +83,6 @@ public class StoryWriteActivity extends AppCompatActivity {
         contentsSequence = new ArrayList <Integer> ();
 
         imageinsertButton = (Button)findViewById(R.id.pickphotoButton);
-
         imageinsertButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -229,6 +230,7 @@ public class StoryWriteActivity extends AppCompatActivity {
         });
     }
 
+    int imageUploadCheck = 0;
     void ImageUpload()
     {
         // **********업로드 코드 ************** 여러개 연속 보낼땐 for문으로 감싸기
@@ -242,7 +244,8 @@ public class StoryWriteActivity extends AppCompatActivity {
             try
             {
                 params.put("image", myFile);
-            } catch (FileNotFoundException e)
+            }
+            catch (FileNotFoundException e)
             {
 
             }
@@ -266,6 +269,7 @@ public class StoryWriteActivity extends AppCompatActivity {
                     //Toast.makeText(getApplicationContext(), new String(response), Toast.LENGTH_LONG).show();
                     //Toast.makeText(getApplicationContext(), "업로드 성공!", Toast.LENGTH_LONG).show();
                     Log.d("ImageUpload", "이미지 업로드 성공!");
+                    imageUploadCheck = 1;
                 }
 
                 @Override
@@ -275,6 +279,11 @@ public class StoryWriteActivity extends AppCompatActivity {
                     //Toast.makeText(getApplicationContext(), new String(errorResponse), Toast.LENGTH_LONG).show();
                     //Toast.makeText(getApplicationContext(), "업로드가 실패했습니다! 다시 시도해주세요!", Toast.LENGTH_LONG).show();
                     Log.d("ImageUpload", "이미지 업로드 실패!");
+                    container.removeView(recyclerView.get(recyclerView.size()-1));
+                    recyclerView.remove(recyclerView.size()-1);
+                    selectedPhotos.remove(selectedPhotos.size()-1);
+                    contentsSequence.remove(contentsSequence.size() - 1);
+                    imageUploadCheck = -1;
                 }
 
                 @Override
@@ -289,7 +298,6 @@ public class StoryWriteActivity extends AppCompatActivity {
 
     void StoryStringUpload()
     {
-
         RequestParams params = new RequestParams();
         params.put("storystring", storystring);
         params.put("seq", 1);
@@ -344,7 +352,10 @@ public class StoryWriteActivity extends AppCompatActivity {
             if (photos != null) // 업로드를 성공적으로 추가하고 돌아온 경우
             {
                 selectedPhotos.get(recyclerView.size()-1).addAll(photos);
-                ImageUpload();
+                //************테스트할때는 주석처리. 과도한 트래픽 막기위해*********
+                //ImageUpload();
+                UploadProgressDialog task = new UploadProgressDialog();
+                task.execute();
             }
             photoAdapter.notifyDataSetChanged();
         }
@@ -352,6 +363,9 @@ public class StoryWriteActivity extends AppCompatActivity {
         if (resultCode == RESULT_CANCELED && (requestCode == PhotoPicker.REQUEST_CODE))
         {
             container.removeView(recyclerView.get(recyclerView.size()-1));
+            recyclerView.remove(recyclerView.size()-1);
+            selectedPhotos.remove(selectedPhotos.size()-1);
+            contentsSequence.remove(contentsSequence.size()-1);
         }
     }
 
@@ -367,7 +381,7 @@ public class StoryWriteActivity extends AppCompatActivity {
         } else {
             // permission denied, boo! Disable the
             // functionality that depends on this permission.
-            Toast.makeText(this, "No read storage permission! Cannot perform the action.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "읽기 권한이 없어 수행할 수가 없습니다. 권한을 설정해주세요.", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -469,6 +483,68 @@ public class StoryWriteActivity extends AppCompatActivity {
                     textTemp++;
                     break;
             }
+        }
+    }
+
+    private class UploadProgressDialog extends AsyncTask<Void, Void, Void>
+    {
+        ProgressDialog asyncDialog = new ProgressDialog(StoryWriteActivity.this);
+
+        @Override
+        protected void onPreExecute()
+        {
+            asyncDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            asyncDialog.setMessage("이미지를 업로드 중입니다. 잠시만 기다려주세요.");
+            asyncDialog.setCanceledOnTouchOutside(false);
+
+            asyncDialog.show();
+            ImageUpload();
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid)
+        {
+            if (imageUploadCheck == 1)
+            {
+                asyncDialog.dismiss();
+                Toast.makeText(getApplicationContext(), "업로드 성공!", Toast.LENGTH_SHORT).show();
+                imageUploadCheck = 0;
+            }
+            else if (imageUploadCheck == -1)
+            {
+                asyncDialog.dismiss();
+                Toast.makeText(getApplicationContext(), "업로드에 실패했습니다. 인터넷 연결을 확인해주세요.", Toast.LENGTH_SHORT).show();
+                imageUploadCheck = 0;
+            }
+            super.onPostExecute(aVoid);
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values)
+        {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params)
+        {
+            /*
+            try
+            {
+                Thread.sleep(300);
+            }
+            catch(InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+            */
+            while(true)
+            {
+                if(imageUploadCheck != 0)
+                    break;
+            }
+            return null;
         }
     }
 
