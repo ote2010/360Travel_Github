6edd6ba70f8599ff4b696f9fc879d8dc0c0f8c94
sunahs <tjsdkz3@naver.com>
2016-09-13 01:50:31 +0900

    Revert "댓글기능 구현"
    This reverts commit 7c6c8d953b6305ebab2ae8d3b229a9d4032fcdb6.

--- Travel360/app/src/main/java/com/example/user/travel360/CustomList/ItemData.java
+++
@@ -1,8 +0,0 @@
-package com.example.user.travel360.CustomList;
-
-public class ItemData {
-  //  public Bitmap comment_img;
-//   public Drawable comment_img;
-    public String comment_id;
-    public String comment_txt;
-}
