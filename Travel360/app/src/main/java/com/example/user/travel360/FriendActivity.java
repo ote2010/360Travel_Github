d595cbd466831bba594db0508516fa3f3c57888c
sunahs <tjsdkz3@naver.com>
2016-09-15 22:53:21 +0900

    친구검색

--- Travel360/app/src/main/java/com/example/user/travel360/FriendActivity.java
+++ Travel360/app/src/main/java/com/example/user/travel360/FriendActivity.java
@@ -3,10 +3,15 @@ package com.example.user.travel360;
 import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.user.travel360.CustomList.CustomAdapter;
@@ -32,10 +37,15 @@ public class FriendActivity extends AppCompatActivity {
     AlertDialog.Builder aDialog;
     AlertDialog ad;
 
+    EditText friendSearchEt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend);
+
+        friendSearchEt = (EditText) findViewById(R.id.friendSearchEt);
+
         listView = (ListView) findViewById(R.id.friendList);
         adapter = new CustomAdapter(itemDatas, FriendActivity.this);
 
@@ -47,51 +57,67 @@ public class FriendActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 ItemData itemData_temp = (ItemData) adapter.getItem(position);
-                 String travleld = itemData_temp.comment_id;
-                 String travleName = itemData_temp.comment_txt;
-                Toast.makeText(FriendActivity.this, "id : "+ travleld + " / name : " + travleName, Toast.LENGTH_SHORT).show();
+                String travleld = itemData_temp.comment_id;
+                String travleName = itemData_temp.comment_txt;
+                Toast.makeText(FriendActivity.this, "id : " + travleld + " / name : " + travleName, Toast.LENGTH_SHORT).show();
             }
+        });
+
+        friendSearchEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {      }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editable.length() <= 0)
+                    getFriendsList_Server();
+                else
+                    getFriendsList_Server(friendSearchEt.getText().toString());
+            }
+
+            @Override // 입력하기 전에
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {       }
         });
     }
 
 
-    /*****************  내 친구 목록  ******************/
+    /*****************
+     * 내 친구 목록
+     ******************/
     void getFriendsList_Server() {
-
+        adapter.clear();
         RequestParams params = new RequestParams();
         // 보내는 data는 seq 만 있으면 됩니다.
-        params.put("seq","1");
+        params.put("seq", "1");
 
         AsyncHttpClient client = new AsyncHttpClient();
 
         Log.d("SUN", "getFriendsList_Server()");
         client.get("http://kibox327.cafe24.com/friendsList.do", params, new AsyncHttpResponseHandler() {
             @Override
-            public void onStart() {  }
+            public void onStart() {   }
 
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] response) {
-                Log.d("SUN", "statusCode : " + statusCode + " , response : " +  new String(response));
+                Log.d("SUN", "statusCode : " + statusCode + " , response : " + new String(response));
                 String res = new String(response);
-                try{
+                try {
                     JSONObject object = new JSONObject(res);
                     String arrayStr = object.get("userDtoList") + "";
                     JSONArray array = new JSONArray(arrayStr);
 
-                    for(int i=0; i<array.length(); i++)
-                    {
-                        JSONObject friend = (JSONObject)array.get(i);
-                        String id = (String)friend.get("id");
-                        String name = (String)friend.get("name");
-                        int seq = (int)friend.get("seq");
-                        Log.d("SUN", "id : "+id + " , seq : "+ seq);
+                    for (int i = 0; i < array.length(); i++) {
+                        JSONObject friend = (JSONObject) array.get(i);
+                        String id = (String) friend.get("id");
+                        String name = (String) friend.get("name");
+                        int seq = (int) friend.get("seq");
+                        Log.d("SUN", "id : " + id + " , seq : " + seq);
                         adapter.addListItem(id, name);
                         listView.setSelection(i);
                     }
 
-                }catch (JSONException e){
+                } catch (JSONException e) {
                     e.printStackTrace();
-                    Log.d("SUN",  "e : " + e.toString());
+                    Log.d("SUN", "e : " + e.toString());
                 }
 
             }
@@ -102,7 +128,60 @@ public class FriendActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onRetry(int retryNo) {  }
+            public void onRetry(int retryNo) {   }
+        });
+    }
+
+
+    /*****************
+     * 내 친구 검색
+     ******************/
+    void getFriendsList_Server(final String friendID) {
+        adapter.clear();
+        RequestParams params = new RequestParams();
+        // 보내는 data는 seq 만 있으면 됩니다.
+        params.put("seq", "1");
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        Log.d("SUN", "getFriendsList_Server(String friendID)");
+        client.get("http://kibox327.cafe24.com/friendsList.do", params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onStart() {   }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                Log.d("SUN", "statusCode : " + statusCode + " , response : " + new String(response));
+                String res = new String(response);
+                try {
+                    JSONObject object = new JSONObject(res);
+                    String arrayStr = object.get("userDtoList") + "";
+                    JSONArray array = new JSONArray(arrayStr);
+
+                    for (int i = 0; i < array.length(); i++) {
+                        JSONObject friend = (JSONObject) array.get(i);
+                        String id = (String) friend.get("id");
+                        String name = (String) friend.get("name");
+                        int seq = (int) friend.get("seq");
+                        if (id.contains(friendID) || name.contains(friendID)) {
+                            Log.d("SUN", "id : " + id + " , name : " + name);
+                            adapter.addListItem(id, name);
+                        }
+                    }
+                    adapter.notifyDataSetChanged();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.d("SUN", "e : " + e.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Log.d("SUN", "onFailure // statusCode : " + statusCode + " , headers : " + headers.toString() + " , error : " + error.toString());
+            }
+
+            @Override
+            public void onRetry(int retryNo) {     }
         });
     }
 }
